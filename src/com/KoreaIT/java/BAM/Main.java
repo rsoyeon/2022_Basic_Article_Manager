a83b2c91518a02abc29a4818369633d6aaf1de79
rsoyeon <fbthdus9325@gmail.com>
2022-07-13 17:28:51 +0900

    명령어 입력

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -8,9 +8,14 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		
-		String command = sc.nextLine();
-		
-		System.out.printf("입력된 명령어: %s\n",command);
+		while (true) {
+			System.out.printf("명령어 ) ");
+			String cmd = sc.nextLine();
+			
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
 		
 		System.out.println("==프로그램끝==");
 		sc.close();
