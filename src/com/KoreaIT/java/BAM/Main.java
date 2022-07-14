47152730b52856c28ac7af0ddd7c5037917c16c9
rsoyeon <fbthdus9325@gmail.com>
2022-07-14 15:58:48 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -1,23 +1,64 @@
 package com.KoreaIT.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==프로그램시작==");
+		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
-		
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
+
 			System.out.printf("명령어 ) ");
-			String cmd = sc.nextLine();
-			
+			String cmd = sc.nextLine().trim();
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
 			if (cmd.equals("exit")) {
 				break;
 			}
+
+			if (cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+
+				System.out.printf("%d번 글이 생성되었습니다\n", id);
+
+			} else if (cmd.equals("article list")) {
+				System.out.println("게시물이 없습니다");
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
 		}
-		
-		System.out.println("==프로그램끝==");
+
+		System.out.println("==프로그램 끝==");
 		sc.close();
+	}
+
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
